a6d2366314ffcee1f264dcdd02773cb3b6829807
Damini Raj <daminiraj70@gmail.com>
2024-07-29 13:41:28 +0530

    Update MyContext.js

--- app/MyContext.js
+++ app/MyContext.js
@@ -1,3 +1,8 @@
 import { createContext } from "react";
 
-export const MyContext = createContext("");
+export const MyContext = createContext<MyContextType>({
+  data: [],
+  setData: () => {},
+  deleteData: [],
+  setDeleteData: () => {},
+});
